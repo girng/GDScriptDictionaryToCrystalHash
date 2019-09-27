3b321203fc69baa4aadf461551c247acdb482b00
I'mAHopelessDev <45474847+girng@users.noreply.github.com>
2019-09-27 02:22:21 -0700

    Update converter.gd

--- converter.gd
+++ converter.gd
@@ -14,8 +14,7 @@ func convert_dict_to_crystal_class(data, class__name):
 	var property_list_s = PoolStringArray()
 	var parameter_list_s = PoolStringArray()
 	var hash_list_s = PoolStringArray()
-	var special_client_modifiers = PoolStringArray()
-	
+
 	for key in data:
 		var obj = data[key]
 		var parameter_values_s = PoolStringArray()
@@ -27,9 +26,6 @@ func convert_dict_to_crystal_class(data, class__name):
 				property_list[key2] = "%s : Hash(String, Int32)" % key2
 				# Conform to Crystal's Hash
 				parameter_values_s.append("%s" % to_json(value).replace(":", "=>"))
-				for mod_key in value:
-					if mod_key.find("client_") != -1:
-						special_client_modifiers.append("property %s = 0" % mod_key.replace("client_", ""))
 			else:
 				#Extract keys
 				property_list[key2] = "%s = \"\" " % key2
@@ -55,9 +51,6 @@ end
 	"%s = Hash(String, %sClass).new" % [class__name, class__name],
 	hash_list_s.join("\n")
 	]
-	
-	if special_client_modifiers.size() > 0:
-		text += "\nclass Client\n%s\nend" % [special_client_modifiers.join("\n")]
 		
 	save_file("%s.cr" % class__name, text)
 	print("Conversion Complete!\n-----------\n%s\n-----------" % text)
